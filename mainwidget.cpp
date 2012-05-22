aa5b805713215baa7db8b3ca1975c82966a6f941
vahid kharazi <vahid@kharazi.net>
2012-05-23 00:27:01 +0430

    -systemtray-

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -1,9 +1,61 @@
+/****************************************************************************
+**
+** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of Nokia Corporation and its Subsidiary(-ies) nor
+**     the names of its contributors may be used to endorse or promote
+**     products derived from this software without specific prior written
+**     permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtGui>
+
 #include "mainwidget.h"
 
-MainWidget::MainWidget(QWidget *parent)
-    : QWidget(parent)
+MainWidget::MainWidget()
 {
+    createIconGroupBox();
+    createActions();
+    createTrayIcon();
 
+    connect(iconComboBox, SIGNAL(currentIndexChanged(int)),
+            this, SLOT(setIcon(int)));
+    connect(trayIcon, SIGNAL(messageClicked()), this, SLOT(messageClicked()));
+    connect(trayIcon, SIGNAL(activated(QSystemTrayIcon::ActivationReason)),
+            this, SLOT(iconActivated(QSystemTrayIcon::ActivationReason)));
+
+    iconComboBox->setCurrentIndex(1);
+    trayIcon->show();
 
     contentsWidget = new QListWidget;
     contentsWidget->setViewMode(QListView::IconMode);
@@ -13,6 +65,9 @@ MainWidget::MainWidget(QWidget *parent)
     contentsWidget->setMaximumWidth(128);
     contentsWidget->setSpacing(10);
     pagesWidget = new QStackedWidget;
+    //
+    //for adding page:
+    //
 //    pagesWidget->addWidget(new ConfigurationPage);
 //    pagesWidget->addWidget(new UpdatePage);
 //    pagesWidget->addWidget(new QueryPage);
@@ -38,11 +93,14 @@ MainWidget::MainWidget(QWidget *parent)
     mainLayout->addSpacing(12);
     mainLayout->addLayout(buttonsLayout);
     setLayout(mainLayout);
-
-    setWindowTitle(QString::fromUtf8("سامانه "));
+    this->resize(600,400);
+    setWindowTitle(QString::fromUtf8("adel jon"));
 }
 
-void MainWidget::createIcons(){
+
+void MainWidget::createIcons()
+{
+    //this function for adding icon to pages
     QListWidgetItem *configButton = new QListWidgetItem(contentsWidget);
     configButton->setIcon(QIcon(":/images/config.png"));
     configButton->setText(QString::fromUtf8("خرید"));
@@ -65,17 +123,118 @@ void MainWidget::createIcons(){
     connect(contentsWidget,
             SIGNAL(currentItemChanged(QListWidgetItem*,QListWidgetItem*)),
             this, SLOT(changePage(QListWidgetItem*,QListWidgetItem*)));
-
 }
-void MainWidget::changePage(QListWidgetItem *current, QListWidgetItem *previous){
+
+void MainWidget::changePage(QListWidgetItem *current, QListWidgetItem *previous)
+{
     if (!current)
         current = previous;
 
     pagesWidget->setCurrentIndex(contentsWidget->row(current));
+}
+
+
+void MainWidget::setVisible(bool visible)
+{
+    QWidget::setVisible(visible);
+}
+
+
+void MainWidget::closeEvent(QCloseEvent *event)
+{
+    if (trayIcon->isVisible()) {
+        hide();
+        event->ignore();
+    }
+}
+
+
+
+void MainWidget::setIcon(int index)
+{
+//seting system tray Icon
+    QIcon icon= QIcon(":/images/bad.svg");//inja icon o vared konim
+    trayIcon->setIcon(icon);
 
 }
 
-MainWidget::~MainWidget()
-{
 
+
+
+void MainWidget::iconActivated(QSystemTrayIcon::ActivationReason reason)
+{
+    switch (reason) {
+    case QSystemTrayIcon::Trigger:
+        qDebug()<<"QSystemTrayIcon::Trigger";
+
+        break;
+    case QSystemTrayIcon::MiddleClick:
+        showMessage("click","middleclick",15);
+        break;
+    default:
+        ;
+    }
+}
+
+
+void MainWidget::showMessage(QString title,QString plaintext,int time=15)
+{
+//for setting messege icon and show a message
+    //tabe ro intori taghir dadam ke se ta parametr begire: QString title,QString plaintext,int time=15
+    //time modat zamaniye k pegham namayesh dade mishe.
+    QSystemTrayIcon::MessageIcon icon = QSystemTrayIcon::MessageIcon(QSystemTrayIcon::Critical);//enum={Information,warning,Critical,noIcon}type icon az inja taghir bedim
+    trayIcon->showMessage(title,plaintext, icon,
+                          time * 1000);
+}
+
+
+void MainWidget::messageClicked()
+{
+    QMessageBox::information(0, tr("Systray"),
+                             tr("Sorry, I already gave what help I could.\n"
+                                "Maybe you should try asking a human?"));
+}
+
+
+void MainWidget::createIconGroupBox()
+{
+    iconGroupBox = new QGroupBox(tr("Tray Icon"));
+    iconComboBox = new QComboBox;
+    iconComboBox->addItem(QIcon(":/images/bad.svg"), tr("bad"));
+}
+
+
+
+
+
+void MainWidget::createActions()
+{
+    //system tray menu action
+    //....w QAction(tr("Mi&nimize"), this)....     you can using from &
+    minimizeAction = new QAction(tr("Mi&nimize"), this);
+    connect(minimizeAction, SIGNAL(triggered()), this, SLOT(hide()));
+
+    maximizeAction = new QAction(tr("Ma&ximize"), this);
+    connect(maximizeAction, SIGNAL(triggered()), this, SLOT(showMaximized()));
+
+    restoreAction = new QAction(QString::fromUtf8("نمایش برنامه"), this);
+    connect(restoreAction, SIGNAL(triggered()), this, SLOT(showNormal()));
+
+    quitAction = new QAction(QString::fromUtf8("خروج"), this);
+    connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+}
+
+
+void MainWidget::createTrayIcon()
+{
+    //system tray menu
+    trayIconMenu = new QMenu(this);
+    trayIconMenu->addAction(minimizeAction);
+    trayIconMenu->addAction(maximizeAction);
+    trayIconMenu->addAction(restoreAction);
+    trayIconMenu->addSeparator();
+    trayIconMenu->addAction(quitAction);
+
+    trayIcon = new QSystemTrayIcon(this);
+    trayIcon->setContextMenu(trayIconMenu);
 }
