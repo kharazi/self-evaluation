1e458de2123e2146ce8591cc6750287f0140aa14
vahid kharazi <vahid@kharazi.net>
2012-05-27 00:38:25 +0430

    record form

--- dashboard.h
+++ dashboard.h
@@ -2,12 +2,16 @@
 #define DASHBOARD_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <QComboBox>
+#include <
 
 class Dashboard : public QWidget
 {
     Q_OBJECT
 public:
     explicit Dashboard(QWidget *parent = 0);
+    Qpush
 
 signals:
 
